a74938e1fd742fbefc94b31c4ea3dcda2d83d00e
Mikhail <wowivan2011@yandex.ru>
2020-10-13 17:01:41 +0300

    Get user info

--- ITLab/UI/Authorize/AuthorizeController.swift
+++ ITLab/UI/Authorize/AuthorizeController.swift
@@ -15,6 +15,21 @@ class AuthorizeController : UIViewController {
     
     private var appAuthInteraction : AppAuthInteraction?
     
+    
+    struct UserInfo: Codable {
+        let userId: UUID
+        
+        public enum CodingKeys: String, CodingKey {
+            case userId = "sub"
+        }
+    }
+    
+    private var userInfo: UserInfo?
+    
+    public func getUserInfo() -> UserInfo? {
+        return self.userInfo
+    }
+    
     public func getAccsesToken() -> String{
         return self.appAuthInteraction?.getAuthState()?.lastTokenResponse?.accessToken ?? ""
     }
@@ -59,6 +74,8 @@ extension AuthorizeController {
     }
     
     func logIn() {
+        getUserInfoReq()
+        
         let menuView = UIHostingController(rootView: MainMenu())
         
         menuView.modalPresentationStyle = .fullScreen
@@ -69,6 +86,88 @@ extension AuthorizeController {
     func performAction(freshTokens: @escaping OIDAuthStateAction)
     {
         appAuthInteraction?.getAuthState()?.performAction(freshTokens: freshTokens)
+    }
+    
+    func getUserInfoReq() {
+        appAuthInteraction?.getAuthState()?.performAction(freshTokens: { (accessToken, _, error) in
+            
+            if error != nil  {
+               print("Error fetching fresh tokens: \(error?.localizedDescription ?? "ERROR")")
+                return
+            }
+            
+            guard let accessToken = accessToken else {
+                print("Error getting accessToken")
+                return
+            }
+            
+            guard let userinfoEndpoint = self.appAuthInteraction?.getAuthState()?.lastAuthorizationResponse.request.configuration.discoveryDocument?.userinfoEndpoint else {
+                print("Userinfo endpoint not declared in discovery document")
+                return
+            }
+            
+            var urlRequest = URLRequest(url: userinfoEndpoint)
+            print(userinfoEndpoint)
+            urlRequest.allHTTPHeaderFields = ["Authorization":"Bearer \(accessToken)"]
+            
+            let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+                
+                DispatchQueue.main.async {
+                    
+                    guard error == nil else {
+                        print("HTTP request failed \(error?.localizedDescription ?? "ERROR")")
+                        return
+                    }
+
+                    guard let response = response as? HTTPURLResponse else {
+                        print("Non-HTTP response")
+                        return
+                    }
+
+                    guard let data = data else {
+                        print("HTTP response data is empty")
+                        return
+                    }
+
+                    var json: [AnyHashable: Any]?
+
+                    do {
+                        json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+                    } catch {
+                        print("JSON Serialization Error")
+                    }
+
+                    
+                    if response.statusCode != 200 {
+                        // server replied with an error
+                        let responseText: String? = String(data: data, encoding: String.Encoding.utf8)
+                        
+                        if response.statusCode == 401 {
+                            // "401 Unauthorized" generally indicates there is an issue with the authorization
+                            // grant. Puts OIDAuthState into an error state.
+                            let oauthError = OIDErrorUtilities.resourceServerAuthorizationError(withCode: 0,
+                                                                                                errorResponse: json,
+                                                                                                underlyingError: error)
+                            self.appAuthInteraction?.getAuthState()?.update(withAuthorizationError: oauthError)
+                            print("Authorization Error (\(oauthError)). Response: \(responseText ?? "RESPONSE_TEXT")")
+                        } else {
+                            print("HTTP: \(response.statusCode), Response: \(responseText ?? "RESPONSE_TEXT")")
+                        }
+
+                        return
+                    }
+                        
+                    guard let user: UserInfo = try? JSONDecoder().decode(UserInfo.self, from: data) else {
+                        print("JSON serialization error in UserInfo")
+                        return
+                    }
+                        self.userInfo = user
+                    print("Success: \(self.userInfo?.userId)")
+                }
+            }
+            
+            task.resume()
+        })
     }
 }
 
