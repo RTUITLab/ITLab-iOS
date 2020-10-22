0c6ff6fa8264bfb1839eab4153f66b65b5d12058
Mikhail <wowivan2011@yandex.ru>
2020-10-22 22:10:03 +0300

    Added config files

--- ITLab/AppAuthInteraction.swift
+++ ITLab/AppAuthInteraction.swift
@@ -10,22 +10,24 @@ import AppAuth
 
 typealias PostRegistrationCallback = (_ configuration: OIDServiceConfiguration?, _ registrationResponse: OIDRegistrationResponse?) -> Void
 
-struct AppAuthConfiguration {
-    static let kIssuer: String = "https://dev.identity.rtuitlab.ru/"
-    static let kClientID: String? = "itlab_mobile_app"
-    static let kRedirectURI: String = "ru.RTUITLab.ITLab:/oauth2redirect/example-provider"
-    static let kAppAuthAuthStateKey: String = "authState"
-}
-
 class AppAuthInteraction: NSObject {
     private var authState: OIDAuthState?
-    
+    private var appAuthConfiguration : AppAuthConfiguration = AppAuthConfiguration()
     private var viewController : AuthorizeController
+    
+    private struct AppAuthConfiguration {
+        var kIssuer: String = ""
+        var kClientID: String = ""
+        var kRedirectURL: String = ""
+        let kAppAuthAuthStateKey: String = "authState"
+    }
     
     init(view: AuthorizeController) {
         self.viewController = view
         
         super.init()
+        
+        self.configurationСheck()
         
         self.loadState()
         self.stateChanged()
@@ -36,6 +38,57 @@ class AppAuthInteraction: NSObject {
         return self.authState
     }
     
+    private func configurationСheck() {
+
+        guard let serverApi = Bundle.main.object(forInfoDictionaryKey: "ServerApi") as? Dictionary<String, String> else {
+            assertionFailure("Server parameters are not specified in info.plist")
+            return
+        }
+        
+        guard var serverURL = serverApi["ServerURL"] else {
+            assertionFailure("No parameter specifying the connection server")
+            return
+        }
+        
+        assert(serverURL != "",
+                "Server reference not specified in config file. Property: API_Issuer");
+        
+        guard let isSecure = serverApi["isSecure"] as NSString? else {
+            assertionFailure("No parameter specifying the connection server")
+            return
+        }
+        
+        if isSecure.boolValue {
+            serverURL = "https://" + serverURL
+        } else {
+            serverURL = "http://" + serverURL
+        }
+        
+        
+        guard let clientName = serverApi["ClientName"] else {
+            assertionFailure("No parameter specifying the connection server.")
+            return
+        }
+        
+        assert(clientName != "",
+                "Client name not specified in config file. Property: API_Client");
+        
+        
+        
+        guard let redirectURL = serverApi["RedirectURL"] else {
+            assertionFailure("No parameter specifying the connection server")
+            return
+        }
+        
+        assert(redirectURL != "",
+                "RedirectURL not specified in config file. API_RedirectURL");
+        
+        self.appAuthConfiguration.kIssuer = serverURL
+        self.appAuthConfiguration.kClientID = clientName
+        self.appAuthConfiguration.kRedirectURL = redirectURL
+        
+    }
+    
 }
 
 extension AppAuthInteraction {
@@ -43,8 +96,8 @@ extension AppAuthInteraction {
     public func authorization() {
         
         
-        guard let issuer = URL(string: AppAuthConfiguration.kIssuer) else {
-            self.logMessage("Error creating URL for : \(AppAuthConfiguration.kIssuer)")
+        guard let issuer = URL(string: self.appAuthConfiguration.kIssuer) else {
+            self.logMessage("Error creating URL for : \(self.appAuthConfiguration.kIssuer)")
             return
         }
 
@@ -58,31 +111,17 @@ extension AppAuthInteraction {
                 self.setAuthState(nil)
                 return
             }
-
+            
             self.logMessage("Got configuration: \(config)")
-
-            if let clientId = AppAuthConfiguration.kClientID {
-                self.doAuthWithAutoCodeExchange(configuration: config, clientID: clientId, clientSecret: nil)
-            } else {
-                self.doClientRegistration(configuration: config) { configuration, response in
-
-                    guard let configuration = configuration, let clientID = response?.clientID else {
-                        self.logMessage("Error retrieving configuration OR clientID")
-                        return
-                    }
-
-                    self.doAuthWithAutoCodeExchange(configuration: configuration,
-                                                    clientID: clientID,
-                                                    clientSecret: response?.clientSecret)
-                }
-            }
+        
+            self.doAuthWithAutoCodeExchange(configuration: config, clientID: self.appAuthConfiguration.kClientID, clientSecret: nil)
         }
     }
     
    public func logOut()
     {
-        guard let issuer = URL(string: AppAuthConfiguration.kIssuer) else {
-            self.logMessage("Error creating URL for : \(AppAuthConfiguration.kIssuer)")
+        guard let issuer = URL(string: self.appAuthConfiguration.kIssuer) else {
+            self.logMessage("Error creating URL for : \(self.appAuthConfiguration.kIssuer)")
             return
         }
 
@@ -112,8 +151,8 @@ extension AppAuthInteraction {
     
     func endSession(configuration: OIDServiceConfiguration)
     {
-        guard let redirectURI = URL(string: AppAuthConfiguration.kRedirectURI) else {
-            self.logMessage("Error creating URL for : \(AppAuthConfiguration.kRedirectURI)")
+        guard let redirectURI = URL(string: self.appAuthConfiguration.kRedirectURL) else {
+            self.logMessage("Error creating URL for : \(self.appAuthConfiguration.kRedirectURL)")
             return
         }
         
@@ -138,40 +177,10 @@ extension AppAuthInteraction {
         
     }
 
-    func doClientRegistration(configuration: OIDServiceConfiguration, callback: @escaping PostRegistrationCallback) {
-
-        guard let redirectURI = URL(string: AppAuthConfiguration.kRedirectURI) else {
-            self.logMessage("Error creating URL for : \(AppAuthConfiguration.kRedirectURI)")
-            return
-        }
-        let request: OIDRegistrationRequest = OIDRegistrationRequest(configuration: configuration,
-                                                                     redirectURIs: [redirectURI],
-                                                                     responseTypes: nil,
-                                                                     grantTypes: nil,
-                                                                     subjectType: nil,
-                                                                     tokenEndpointAuthMethod: "client_secret_post",
-                                                                     additionalParameters: nil)
-
-        // performs registration request
-        self.logMessage("Initiating registration request")
-
-        OIDAuthorizationService.perform(request) { response, error in
-
-            if let regResponse = response {
-                self.setAuthState(OIDAuthState(registrationResponse: regResponse))
-                self.logMessage("Got registration response: \(regResponse)")
-                callback(configuration, regResponse)
-            } else {
-                self.logMessage("Registration error: \(error?.localizedDescription ?? "DEFAULT_ERROR")")
-                self.setAuthState(nil)
-            }
-        }
-    }
-
     func doAuthWithAutoCodeExchange(configuration: OIDServiceConfiguration, clientID: String, clientSecret: String?) {
 
-        guard let redirectURI = URL(string: AppAuthConfiguration.kRedirectURI) else {
-            self.logMessage("Error creating URL for : \(AppAuthConfiguration.kRedirectURI)")
+        guard let redirectURI = URL(string: self.appAuthConfiguration.kRedirectURL) else {
+            self.logMessage("Error creating URL for : \(self.appAuthConfiguration.kRedirectURL)")
             return
         }
 
@@ -235,7 +244,7 @@ extension AppAuthInteraction {
         }
         
         if let userDefaults = UserDefaults(suiteName: "group.ru.RTUITLab.ITLab") {
-            userDefaults.set(data, forKey: AppAuthConfiguration.kAppAuthAuthStateKey)
+            userDefaults.set(data, forKey: self.appAuthConfiguration.kAppAuthAuthStateKey)
 //            print(data)
             userDefaults.synchronize()
         }
@@ -243,7 +252,7 @@ extension AppAuthInteraction {
 
     func loadState() {
         guard let data = UserDefaults (suiteName: "group.ru.RTUITLab.ITLab")?
-                .object(forKey: AppAuthConfiguration.kAppAuthAuthStateKey) as? Data
+                .object(forKey: self.appAuthConfiguration.kAppAuthAuthStateKey) as? Data
         else {
             return
         }
