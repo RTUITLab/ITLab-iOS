c9526a23ef4a496298ecfa3f86298a15877f14c2
Mikhail <wowivan2011@yandex.ru>
2020-11-16 11:08:58 +0300

    Added information about the equipment on hand

--- ITLab/UI/User/UserPage.swift
+++ ITLab/UI/User/UserPage.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct UserPage: View {
     @State var user: UserView
-//    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     @State var equipments: [EquipmentView] = []
     @State var isLoadingEquipments: Bool = true
@@ -149,167 +148,35 @@ struct UserPage: View {
                     }
                 }
             }
+            
+            Section(header: Text("Техника на руках")) {
+                if isLoadingEquipments {
+                    GeometryReader() { g in
+                        ProgressView()
+                            .frame(width: g.size.width, height: g.size.height, alignment: .center)
+                        
+                    }
+                } else {
+                    if equipments.count > 0 {
+                        VStack (alignment: .leading) {
+                            ForEach(equipments, id: \._id) {
+                                equipment in
+                                
+                                EquipmentStack(equipment: equipment)
+                            }
+                        }
+                    } else {
+                        Text("Оборудование на руках нет")
+                    }
+                }
+            }
+            .onAppear() {
+                getEquimpment()
+            }
         }
         .listStyle(GroupedListStyle())
         .navigationBarTitleDisplayMode(.inline)
-        .onAppear() {
-        }
     }
-    
-//    var body: some View {
-//        VStack (alignment: .leading) {
-//            Button (action: {
-//                presentationMode.wrappedValue.dismiss()
-//            }, label: {
-//                HStack {
-//                    Image(systemName: "chevron.left")
-//                        .font(Font.title2.weight(.medium))
-//
-//                    Text("Пользователи")
-//                        .font(.system(size: 17))
-//                }
-//            })
-//            .padding(.horizontal, 10)
-//            .padding(.top, 12.0)
-//            VStack (alignment: .leading) {
-//                Text("\(user.lastName ?? "") \(user.firstName ?? "") \(user.middleName ?? "")")
-//                    .font(.title)
-//                    .fontWeight(.bold)
-//
-//                Divider()
-//
-//                VStack(alignment: .leading) {
-//                    HStack(spacing: 20) {
-//                        VStack(alignment: .leading, spacing: 10) {
-//                            if user.email != nil {
-//                                Text("Email:")
-//                                    .font(.title3)
-//                            }
-//
-//                            if user.phoneNumber != nil {
-//                                Text("Телефон:")
-//                                    .font(.title3)
-//                            }
-//                        }
-//
-//                        VStack(alignment: .leading, spacing: 10) {
-//                            if user.email != nil {
-//                                Text(user.email!)
-//                                    .foregroundColor(.blue)
-//                                    .font(.title3)
-//                                    .contextMenu() {
-//                                        Button(action: {
-//                                            if let email = user.email {
-//                                                UIApplication.shared.open(URL(string: "mailto://compose?to=\(email)")!)
-//                                            }
-//                                        }) {
-//                                            Text("Отправить письмо")
-//                                            Image(systemName: "square.and.pencil")
-//                                        }
-//
-//                                        Button(action: {
-//                                            UIPasteboard.general.string = user.email!
-//                                        }) {
-//                                            Text("Копировать")
-//                                            Image(systemName: "doc.on.doc")
-//                                        }
-//                                    }
-//                                    .onTapGesture {
-//                                        if let email = user.email {
-//                                            UIApplication.shared.open(URL(string: "mailto://compose?to=\(email)")!)
-//                                        }
-//
-//                                    }
-//                            }
-//
-//                            if user.phoneNumber != nil {
-//                                Text(user.phoneNumber!)
-//                                    .foregroundColor(.blue)
-//                                    .font(.title3)
-//                                    .contextMenu() {
-//                                        Button(action: {
-//                                            if var phone : String = user.phoneNumber {
-//                                                let regex = try! NSRegularExpression(pattern: "[^0-9]")
-//                                                phone = regex.stringByReplacingMatches(in: phone, options: [], range: NSRange(0..<phone.utf8.count), withTemplate: "")
-//
-//                                                UIApplication.shared.open(URL(string: "tel://\(phone)")!)
-//                                            }
-//                                        }) {
-//                                            Text("Набрать номер")
-//                                            Image(systemName: "phone")
-//                                        }
-//
-//                                        Button(action: {
-//                                            UIPasteboard.general.string = user.phoneNumber!
-//                                        }) {
-//                                            Text("Копировать")
-//                                            Image(systemName: "doc.on.doc")
-//                                        }
-//                                    }
-//                                    .onTapGesture {
-//                                        if var phone : String = user.phoneNumber {
-//                                            let regex = try! NSRegularExpression(pattern: "[^0-9]")
-//                                            phone = regex.stringByReplacingMatches(in: phone, options: [], range: NSRange(0..<phone.utf8.count), withTemplate: "")
-//
-//                                            UIApplication.shared.open(URL(string: "tel://\(phone)")!)
-//                                        }
-//                                    }
-//                            }
-//                        }
-//                    }
-//                }
-//
-//                Divider()
-//
-//                ScrollView() {
-//                    VStack(alignment: .leading) {
-//                        VStack (alignment: .leading) {
-//                            Text("Оборудование")
-//                                .font(.title3)
-//                                .fontWeight(.bold)
-//                                .padding(.bottom, 5)
-//
-//                            if isLoadingEquipments {
-//                                ProgressView()
-//                                    .padding(.vertical, 15)
-//                                    .padding(.horizontal, (UIScreen.main.bounds.width / 2) - 10)
-//                            } else {
-//
-//                                if equipments.count > 0 {
-//                                    VStack (alignment: .leading) {
-//                                        ForEach(equipments, id: \._id) {
-//                                            equipment in
-//
-//                                            EquipmentStack(equipment: equipment)
-//                                        }
-//                                    }
-//                                } else {
-//                                    Text("Оборудование на руках нет")
-//                                }
-//                            }
-//                        }
-//
-//                        Divider().padding(.top, 10)
-//
-//                        VStack (alignment: .leading) {
-//                            Text("Участие в событиях")
-//                                .font(.title3)
-//                                .fontWeight(.bold)
-//                                .padding(.bottom, 5)
-//                        }
-//                    }
-//                }
-//                //                Spacer()
-//            }
-//            .padding(.horizontal, 20.0)
-//            .padding(.top, 10)
-//        }
-//        .frame(width: UIScreen.main.bounds.width, alignment: .leading)
-//        .navigationBarHidden(true)
-//        .onAppear() {
-//            getEquimpment()
-//        }
-//    }
     
     func getEquimpment() {
         AppAuthInteraction.shared.performAction { (token, _) in
@@ -340,7 +207,10 @@ extension UserPage {
                     .fontWeight(.semibold)
                     .padding(.bottom, 2)
                 
-                Text(equipment.serialNumber!)
+                HStack(alignment: .center) {
+                    Text("S/N:")
+                    Text(equipment.serialNumber!)
+                }
             } .padding(5)
         }
     }
