5a7882c60feb7efbfb3390e8a348c84eebd8d711
Mikhail <wowivan2011@yandex.ru>
2021-01-19 05:53:32 +0300

    change scope

--- ITLab/UI/MainMenu.swift
+++ ITLab/UI/MainMenu.swift
@@ -93,20 +93,32 @@ struct СolorPaletteView: View {
                         Spacer()
                         
                         Text("1")
+                            .fontWeight(.bold)
                             .frame(width: g.size.width / 4, height: g.size.width / 4)
                             .background(Rectangle()
                                             .foregroundColor(oneButtonColor))
+                            .onTapGesture {
+                                changeScope(oneButtonColor)
+                            }
                         
                         Text("2")
+                            .fontWeight(.bold)
                             .frame(width: g.size.width / 4, height: g.size.width / 4)
                             .background(Rectangle()
                                             .foregroundColor(twoButtonColor))
                             .padding(.horizontal)
+                            .onTapGesture {
+                                changeScope(twoButtonColor)
+                            }
                         
                         Text("3")
+                            .fontWeight(.bold)
                             .frame(width: g.size.width / 4, height: g.size.width / 4)
                             .background(Rectangle()
                                             .foregroundColor(threeButtonColor))
+                            .onTapGesture {
+                                changeScope(threeButtonColor)
+                            }
                         
                         Spacer()
                     }
@@ -123,13 +135,49 @@ struct СolorPaletteView: View {
             .frame(height: totalHeight)
             .padding(.bottom, 20.0)
         }
+        .onAppear() {
+            changeColor()
+        }
     }
     
-    func generateRandomColor() -> UIColor {
+    func generateColor() -> UIColor {
         let hue : CGFloat = CGFloat(arc4random() % 256) / 256 // use 256 to get full range from 0.0 to 1.0
         let saturation : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5 // from 0.5 to 1.0 to stay away from white
         let brightness : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5 // from 0.5 to 1.0 to stay away from black
         
         return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
+    }
+    
+    func changeColor() {
+         mainColor = Color(generateColor())
+         let randomButton = arc4random_uniform(3)
+        switch randomButton {
+         case 0:
+             oneButtonColor = mainColor
+             twoButtonColor = Color(generateColor())
+             threeButtonColor = Color(generateColor())
+         case 1:
+            oneButtonColor = Color(generateColor())
+            twoButtonColor = mainColor
+            threeButtonColor = Color(generateColor())
+         case 2:
+            oneButtonColor = Color(generateColor())
+            twoButtonColor = Color(generateColor())
+            threeButtonColor = mainColor
+         default:
+            oneButtonColor = mainColor
+            twoButtonColor = Color(generateColor())
+            threeButtonColor = Color(generateColor())
+         }
+    }
+    
+    func changeScope (_ color: Color) {
+        if mainColor == color {
+            scope += 1
+        } else {
+            scope -= 1
+        }
+        
+        changeColor()
     }
 }
