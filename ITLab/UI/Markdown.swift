3be6b85b1407b53b2602494539b3c0a818729c22
Mikhail <wowivan2011@yandex.ru>
2021-05-25 23:31:10 +0300

    Added markdown style

--- ITLab/UI/Markdown.swift
+++ ITLab/UI/Markdown.swift
@@ -17,7 +17,8 @@ struct Markdown: UIViewRepresentable {
     init(text: String, height: Binding<CGFloat>) {
         self._dynamicHeight = height
         let down = Down(markdownString: text)
-        self.attributedText = try? down.toAttributedString()
+        
+        self.attributedText = try? down.toAttributedString(styler: ITLabStyler())
     }
     
     func makeUIView(context: Context) -> UITextView {
@@ -37,10 +38,32 @@ struct Markdown: UIViewRepresentable {
     func updateUIView(_ uiView: UITextView, context: Context) {
         uiView.attributedText = attributedText
         uiView.textColor = colorScheme == .dark ? UIColor.white : UIColor.black
-            DispatchQueue.main.async {
-                dynamicHeight = uiView.sizeThatFits(CGSize(width: uiView.bounds.width,
-                                                           height: CGFloat.greatestFiniteMagnitude))
-                    .height
+        DispatchQueue.main.async {
+            dynamicHeight = uiView.sizeThatFits(CGSize(width: uiView.bounds.width,
+                                                       height: CGFloat.greatestFiniteMagnitude))
+                .height
+        }
+    }
+}
+
+// TODO: Waiting for my PR to be accepted into the Down library repository
+final class ITLabStyler: DownStyler {
+    override func style(image str: NSMutableAttributedString, title: String?, url: String?) {
+        if let urlImg = URL(string: url!) {
+            let semaphore = DispatchSemaphore(value: 0)
+            URLSession.shared.dataTask(with: urlImg) { data, _, _ in
+                if let data = data {
+                    let image1Attachment = NSTextAttachment()
+                    image1Attachment.image = UIImage(data: data)
+                    let image1String = NSAttributedString(attachment: image1Attachment)
+                    
+                    str.setAttributedString(image1String)
+                }
+                
+                semaphore.signal()
+            }.resume()
+            
+            semaphore.wait()
         }
     }
 }
