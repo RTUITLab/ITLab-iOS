78837f6b8b3fc8fd44a24ae843022bd3249a56e8
0B1t322 <dan.demin@gmail.com>
2021-07-10 22:28:21 +0300

    add searchbar and toggle toggle for equipment list

--- ITLab/UI/Equipment/EquipmentList.swift
+++ ITLab/UI/Equipment/EquipmentList.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct EquipmentList: View {
+    @State private var searchText = ""
+    @State private var onlyFree = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            EquipmentSearchBar(text: $searchText, placehodler: "Поиск оборудования")
+            HStack {
+                Toggle(
+                    isOn: $onlyFree,
+                    label: {
+                        Text("Только свободное")
+                            .font(.system(size: 12, weight: .light, design: .default))
+                    }
+                )
+                .toggleStyle(
+                    CheckBoxStyle()
+                )
+                
+                Spacer()
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+struct CheckBoxStyle: ToggleStyle {
+    var width: CGFloat = 20
+    var height: CGFloat = 20
+    func makeBody(configuration: Self.Configuration) -> some View {
+        return HStack {
+            Image(systemName: configuration.isOn ? "checkmark.circle.fill" : "circle" )
+                .frame(width: width, height: height)
+                .foregroundColor(configuration.isOn ? .green : .gray)
+                .font(.system(size: 20, weight: .bold, design: .default))
+                .onTapGesture {
+                    configuration.isOn.toggle()
+                }
+            
+            configuration.label
+        }
     }
 }
 
