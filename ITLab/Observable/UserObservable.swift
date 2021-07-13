a3516f98966ab692f1826aaf3cf07fda95b40b8b
0B1t322 <dan.demin@gmail.com>
2021-07-13 14:44:06 +0300

    Add user observable

---
+++ ITLab/Observable/UserObservable.swift
@@ -0,0 +1,42 @@
+//
+//  UserObservable.swift
+//  ITLab
+//
+//  Created by Даня Демин on 12.07.2021.
+//
+
+import Foundation
+import SwiftUI
+
+final class UserObservable: ObservableObject {
+    @Published var isLoading: Bool = false
+    @Published var user: UserView? = nil
+    
+    #if targetEnvironment(simulator)
+    func getUser(userId: String) {
+        if userId == "some_user_id_1" {
+            self.user = UserView(
+                _id: UUID(),
+                firstName: "UserName",
+                lastName: "LastName",
+                middleName: nil,
+                phoneNumber: nil,
+                email: "some_email",
+                properties: nil
+            )
+        }
+    }
+    #else
+    func getUser(userId: String) {
+        // Api stuff
+    }
+    #endif
+    
+    func getFullName() -> String? {
+        if user?.firstName == nil || user?.lastName == nil {
+            return nil
+        } else {
+            return "\(user?.firstName!) \(user?.lastName)"
+        }
+    }
+}
