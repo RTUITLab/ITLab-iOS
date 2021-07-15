45a4ee98c8674f5dba1a041fea5ae73e595b810f
0B1t322 <dan.demin@gmail.com>
2021-07-15 16:57:08 +0300

    add equipment page

--- ITLab/UI/Equipment/EquipmentPage.swift
+++ ITLab/UI/Equipment/EquipmentPage.swift
@@ -8,13 +8,56 @@
 import SwiftUI
 
 struct EquipmentPage: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    enum ChoosenPage: String, CaseIterable {
+        case equipment = "Оборудование"
+        case addEquipment = "Добавить оборудование"
     }
+    
+    @State var pickers: ChoosenPage = .equipment
+    @ObservedObject var equipmentObservable = EquipmentsObservable()
+    
+    var body: some View {
+        VStack {
+            Picker(
+                selection: $pickers,
+                label: Text("Picker"),
+                content: {
+                    ForEach(
+                        ChoosenPage.allCases,
+                        id: \.self
+                    ) {
+                        Text($0.rawValue).textCase(.none)
+                    }
+                }
+            ).pickerStyle(SegmentedPickerStyle())
+            Spacer()
+            content
+        }
+        
+    }
+    
+    var content: some View {
+        Group {
+            switch self.pickers {
+            case .equipment:
+                EquipmentList()
+                    .environmentObject(self.equipmentObservable)
+            case .addEquipment:
+                EquipmentScanView()
+            }
+        }
+    }
+    
+    func loadData() {
+        self.equipmentObservable.getEquipment()
+    }
+    
 }
 
 struct EquipmentPage_Previews: PreviewProvider {
     static var previews: some View {
-        EquipmentPage()
+        var page = EquipmentPage()
+        page.loadData()
+        return page
     }
 }
