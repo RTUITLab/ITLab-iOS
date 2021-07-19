9b6a40ce8ddb0742201be0d1d2234fb00ad96d01
0B1t322 <dan.demin@gmail.com>
2021-07-19 14:50:01 +0300

    add observable objects

--- ITLab/Observable/Equipment/EquipmentObservable.swift
+++ ITLab/Observable/Equipment/EquipmentObservable.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+final class EquipmentObservable: ObservableObject {
+    @Published var equipment: EquipmentModel? = nil
+    
+    #if targetEnvironment(simulator)
+    func createEquipment() {
+        print("created")
+    }
+    #else
+    func createEquipment() {
+        
+    }
+    #endif
+}
--- ITLab/Observable/Equipment/EquipmentTypeObservable.swift
+++ ITLab/Observable/Equipment/EquipmentTypeObservable.swift
@@ -9,4 +9,16 @@ import Foundation
 
 class EquipmentTypeObservable: ObservableObject {
     @Published var equipmentType: EquipmentTypeModel? = nil
+    
+    /// Возврашает EquipmentTypeID
+    #if targetEnvironment(simulator)
+    func createType() -> String {
+        return "some_EquipmentTypeID"
+    }
+    #else
+    func createType() -> String {
+        // some api stuff
+        return ""
+    }
+    #endif
 }
